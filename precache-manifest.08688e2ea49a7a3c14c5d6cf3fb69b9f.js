43c91cb1e862aede35df22d754eefc0b83b1622c
njuhi <morphynjuh@mail.com>
2019-04-03 12:44:37 +0300

    Updates

---
+++ precache-manifest.08688e2ea49a7a3c14c5d6cf3fb69b9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc07397a2b16de7a2041",
+    "url": "/myreact/static/css/main.fda53b74.chunk.css"
+  },
+  {
+    "revision": "fc07397a2b16de7a2041",
+    "url": "/myreact/static/js/main.17245506.chunk.js"
+  },
+  {
+    "revision": "a1a1d8a90cb3c44363e9",
+    "url": "/myreact/static/js/runtime~main.b769e414.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/myreact/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "982551910f6840c365f338496338f964",
+    "url": "/myreact/index.html"
+  }
+];
